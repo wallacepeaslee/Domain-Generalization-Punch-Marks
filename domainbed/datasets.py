b1391081309806e1ac7056d6bd2419bda4f63eb8
wallacepeaslee <104286441+wallacepeaslee@users.noreply.github.com>
2024-10-29 18:00:09 +0000

    Add punch mark dataset

--- domainbed/datasets.py
+++ domainbed/datasets.py
@@ -9,8 +9,8 @@ from torch.utils.data import TensorDataset, Subset, ConcatDataset, Dataset
 from torchvision.datasets import MNIST, ImageFolder
 from torchvision.transforms.functional import rotate
 
-from wilds.datasets.camelyon17_dataset import Camelyon17Dataset
-from wilds.datasets.fmow_dataset import FMoWDataset
+# from wilds.datasets.camelyon17_dataset import Camelyon17Dataset
+# from wilds.datasets.fmow_dataset import FMoWDataset
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -29,8 +29,8 @@ DATASETS = [
     "DomainNet",
     "SVIRO",
     # WILDS datasets
-    "WILDSCamelyon",
-    "WILDSFMoW",
+    # "WILDSCamelyon",
+    # "WILDSFMoW",
     # Spawrious datasets
     "SpawriousO2O_easy",
     "SpawriousO2O_medium",
@@ -224,6 +224,74 @@ class MultipleEnvironmentImageFolder(MultipleDomainDataset):
         self.input_shape = (3, 224, 224,)
         self.num_classes = len(self.datasets[-1].classes)
 
+
+
+class PunchMarkMultipleEnvironmentImageFolder(MultipleDomainDataset):
+    '''
+    Copied from MultipleEnvirontmentImageFolder, with a different augment_transform
+    '''
+    def __init__(self, root, test_envs, augment, hparams):
+        super().__init__()
+        environments = [f.name for f in os.scandir(root) if f.is_dir()]
+        environments = sorted(environments)
+
+        transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        augment_transform = transforms.Compose([
+            # transforms.Resize((224,224)),
+            transforms.RandomResizedCrop(224, scale=(0.9, 1.0), ratio = (0.95, 1.05)),
+            transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
+            transforms.RandomRotation(9),
+            transforms.RandomGrayscale(0.4),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        self.datasets = []
+        for i, environment in enumerate(environments):
+
+            if augment and (i not in test_envs):
+                env_transform = augment_transform
+            else:
+                env_transform = transform
+
+            path = os.path.join(root, environment)
+            env_dataset = ImageFolder(path,
+                transform=env_transform)
+
+            self.datasets.append(env_dataset)
+
+        self.input_shape = (3, 224, 224,)
+        self.num_classes = len(self.datasets[-1].classes) 
+
+class PunchMarksDefault(PunchMarkMultipleEnvironmentImageFolder):
+    N_STEPS = 200
+    CHECKPOINT_FREQ = 2
+    N_WORKERS = 1
+    def __init__(self, root, test_envs, hparams):
+        dataset_filepath = '' #TODO: add relevant dataset filepath
+        dataset_folder_name = '' #TODO: add relevant folder name
+        self.dir = os.path.join(dataset_filepath, dataset_folder_name)
+        super().__init__(self.dir, test_envs, hparams['data_augmentation'], hparams)
+        
+class PunchMarksSweep(PunchMarkMultipleEnvironmentImageFolder):
+    N_STEPS = 400
+    CHECKPOINT_FREQ = 5
+    N_WORKERS = 1
+    def __init__(self, root, test_envs, hparams):
+        dataset_filepath = '' #TODO: add relevant dataset filepath
+        dataset_folder_name = '' #TODO: add relevant folder name
+        self.dir = os.path.join(dataset_filepath, dataset_folder_name)
+        super().__init__(self.dir, test_envs, hparams['data_augmentation'], hparams)
+
+
+
 class VLCS(MultipleEnvironmentImageFolder):
     CHECKPOINT_FREQ = 300
     ENVIRONMENTS = ["C", "L", "S", "V"]
@@ -267,100 +335,100 @@ class SVIRO(MultipleEnvironmentImageFolder):
         super().__init__(self.dir, test_envs, hparams['data_augmentation'], hparams)
 
 
-class WILDSEnvironment:
-    def __init__(
-            self,
-            wilds_dataset,
-            metadata_name,
-            metadata_value,
-            transform=None):
-        self.name = metadata_name + "_" + str(metadata_value)
+# class WILDSEnvironment:
+#     def __init__(
+#             self,
+#             wilds_dataset,
+#             metadata_name,
+#             metadata_value,
+#             transform=None):
+#         self.name = metadata_name + "_" + str(metadata_value)
 
-        metadata_index = wilds_dataset.metadata_fields.index(metadata_name)
-        metadata_array = wilds_dataset.metadata_array
-        subset_indices = torch.where(
-            metadata_array[:, metadata_index] == metadata_value)[0]
+#         metadata_index = wilds_dataset.metadata_fields.index(metadata_name)
+#         metadata_array = wilds_dataset.metadata_array
+#         subset_indices = torch.where(
+#             metadata_array[:, metadata_index] == metadata_value)[0]
 
-        self.dataset = wilds_dataset
-        self.indices = subset_indices
-        self.transform = transform
+#         self.dataset = wilds_dataset
+#         self.indices = subset_indices
+#         self.transform = transform
 
-    def __getitem__(self, i):
-        x = self.dataset.get_input(self.indices[i])
-        if type(x).__name__ != "Image":
-            x = Image.fromarray(x)
+#     def __getitem__(self, i):
+#         x = self.dataset.get_input(self.indices[i])
+#         if type(x).__name__ != "Image":
+#             x = Image.fromarray(x)
 
-        y = self.dataset.y_array[self.indices[i]]
-        if self.transform is not None:
-            x = self.transform(x)
-        return x, y
+#         y = self.dataset.y_array[self.indices[i]]
+#         if self.transform is not None:
+#             x = self.transform(x)
+#         return x, y
 
-    def __len__(self):
-        return len(self.indices)
-
-
-class WILDSDataset(MultipleDomainDataset):
-    INPUT_SHAPE = (3, 224, 224)
-    def __init__(self, dataset, metadata_name, test_envs, augment, hparams):
-        super().__init__()
-
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-
-        augment_transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.RandomResizedCrop(224, scale=(0.7, 1.0)),
-            transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
-            transforms.RandomGrayscale(),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-
-        self.datasets = []
-
-        for i, metadata_value in enumerate(
-                self.metadata_values(dataset, metadata_name)):
-            if augment and (i not in test_envs):
-                env_transform = augment_transform
-            else:
-                env_transform = transform
-
-            env_dataset = WILDSEnvironment(
-                dataset, metadata_name, metadata_value, env_transform)
-
-            self.datasets.append(env_dataset)
-
-        self.input_shape = (3, 224, 224,)
-        self.num_classes = dataset.n_classes
-
-    def metadata_values(self, wilds_dataset, metadata_name):
-        metadata_index = wilds_dataset.metadata_fields.index(metadata_name)
-        metadata_vals = wilds_dataset.metadata_array[:, metadata_index]
-        return sorted(list(set(metadata_vals.view(-1).tolist())))
+#     def __len__(self):
+#         return len(self.indices)
 
 
-class WILDSCamelyon(WILDSDataset):
-    ENVIRONMENTS = [ "hospital_0", "hospital_1", "hospital_2", "hospital_3",
-            "hospital_4"]
-    def __init__(self, root, test_envs, hparams):
-        dataset = Camelyon17Dataset(root_dir=root)
-        super().__init__(
-            dataset, "hospital", test_envs, hparams['data_augmentation'], hparams)
+# class WILDSDataset(MultipleDomainDataset):
+#     INPUT_SHAPE = (3, 224, 224)
+#     def __init__(self, dataset, metadata_name, test_envs, augment, hparams):
+#         super().__init__()
+
+#         transform = transforms.Compose([
+#             transforms.Resize((224, 224)),
+#             transforms.ToTensor(),
+#             transforms.Normalize(
+#                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+#         ])
+
+#         augment_transform = transforms.Compose([
+#             transforms.Resize((224, 224)),
+#             transforms.RandomResizedCrop(224, scale=(0.7, 1.0)),
+#             transforms.RandomHorizontalFlip(),
+#             transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
+#             transforms.RandomGrayscale(),
+#             transforms.ToTensor(),
+#             transforms.Normalize(
+#                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#         ])
+
+#         self.datasets = []
+
+#         for i, metadata_value in enumerate(
+#                 self.metadata_values(dataset, metadata_name)):
+#             if augment and (i not in test_envs):
+#                 env_transform = augment_transform
+#             else:
+#                 env_transform = transform
+
+#             env_dataset = WILDSEnvironment(
+#                 dataset, metadata_name, metadata_value, env_transform)
+
+#             self.datasets.append(env_dataset)
+
+#         self.input_shape = (3, 224, 224,)
+#         self.num_classes = dataset.n_classes
+
+#     def metadata_values(self, wilds_dataset, metadata_name):
+#         metadata_index = wilds_dataset.metadata_fields.index(metadata_name)
+#         metadata_vals = wilds_dataset.metadata_array[:, metadata_index]
+#         return sorted(list(set(metadata_vals.view(-1).tolist())))
 
 
-class WILDSFMoW(WILDSDataset):
-    ENVIRONMENTS = [ "region_0", "region_1", "region_2", "region_3",
-            "region_4", "region_5"]
-    def __init__(self, root, test_envs, hparams):
-        dataset = FMoWDataset(root_dir=root)
-        super().__init__(
-            dataset, "region", test_envs, hparams['data_augmentation'], hparams)
+# class WILDSCamelyon(WILDSDataset):
+#     ENVIRONMENTS = [ "hospital_0", "hospital_1", "hospital_2", "hospital_3",
+#             "hospital_4"]
+#     def __init__(self, root, test_envs, hparams):
+#         dataset = Camelyon17Dataset(root_dir=root)
+#         super().__init__(
+#             dataset, "hospital", test_envs, hparams['data_augmentation'], hparams)
+
+
+# class WILDSFMoW(WILDSDataset):
+#     ENVIRONMENTS = [ "region_0", "region_1", "region_2", "region_3",
+#             "region_4", "region_5"]
+#     def __init__(self, root, test_envs, hparams):
+#         dataset = FMoWDataset(root_dir=root)
+#         super().__init__(
+#             dataset, "region", test_envs, hparams['data_augmentation'], hparams)
 
 
 ## Spawrious base classes
