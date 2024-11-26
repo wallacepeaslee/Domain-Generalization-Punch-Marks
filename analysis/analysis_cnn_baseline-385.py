d04106aaafd4d0dcca79269b4cc654ff8c06fa52
wallacepeaslee <104286441+wallacepeaslee@users.noreply.github.com>
2024-11-26 02:01:40 +0000

    Add data processing

---
+++ analysis/analysis_cnn_baseline-385.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 11:33:33 2024
+
+@author: walla
+"""
+
+import numpy as np
+import os
+
+def Stats(d):
+    data = np.array(d)
+    mean = 100 * np.mean(list(data))
+    err = 100 * np.std(list(data) / np.sqrt(len(data)))
+    return mean, err
+
+
+
+def getDataDict(fp):
+    res = {}
+    currentEpoch = None
+    bestEpoch = None
+    bestAcc = None
+    with open(fp, 'r') as f:
+        for line in f:
+            if 'Epoch:' in line:
+                currentEpoch = int(line.strip().split(' ')[-1])
+                res[currentEpoch] = {}
+            if currentEpoch is not None:
+                if 'valid' in line:
+                    res[currentEpoch]['valid'] = float(line.strip().split(' ')[-1])
+                if 'mcb' in line:
+                    res[currentEpoch]['mcb'] = float(line.strip().split(' ')[-1])
+                if 'fitz' in line:
+                    res[currentEpoch]['fitz'] = float(line.strip().split(' ')[-1])
+                if 'cologne' in line:
+                    res[currentEpoch]['cologne'] = float(line.strip().split(' ')[-1])
+                if '385' in line:
+                    res[currentEpoch]['385'] = float(line.strip().split(' ')[-1])
+            if 'best epoch' in line:
+                bestEpoch = int(line.strip().split(' ')[-1])
+            if 'best accuracy' in line:
+                # print(line.strip().split(',')[-1])
+                bestAcc = float(line.strip().split(',')[0].split('(')[1])
+    return res, bestEpoch, bestAcc
+
+
+def getDictMeans(dList):
+    res = {}
+    for elm in dList[0].keys():
+        vals = []
+        for d in dList:
+            vals.append(d[elm])
+        res[elm] = sum(vals)/len(vals)
+    return res
+            
+
+def getStats(algDir):
+    exps = os.listdir(algDir)
+    trials = [[0 for _ in range(3)] for _ in range(3)]
+    for exp in exps:
+        expPath = os.path.join(algDir, exp)
+        d, bestE, bestA = getDataDict(expPath)
+        oracleAcc = d[19]['mcb']
+        splitNum = int(exp.strip().split('_')[1])
+        repNum = int(exp.strip().split('_')[3])
+        trials[splitNum][repNum] = (d, (bestA, bestE), (oracleAcc, 19))
+    
+    
+    #sorry for super unreadable code, I'm tired today
+    oracleBest = [0,0,0]
+    validBest = [0,0,0]
+    for i in range(3):
+        for j in range(3):
+            if trials[i][j][2][0] > oracleBest[i]:
+                oracleBest[i] = trials[i][j][2][0]
+            if trials[i][j][1][0] > validBest[i]:
+                validBest[i] = trials[i][j][1][0]
+    
+    
+    resDataValid = []
+    resDataOracle = []
+    for i in range(3):
+        currTrialsValid = []
+        currTrialsOracle = []
+        for j in range(3):
+            if trials[i][j][1][0] == validBest[i]:
+                currTrialsValid.append((trials[i][j][0], trials[i][j][1][1]))
+            if trials[i][j][2][0] == oracleBest[i]:
+                currTrialsOracle.append((trials[i][j][0], trials[i][j][2][1]))
+        validList = [x[0][x[1]] for x in currTrialsValid]
+        oracleList = [x[0][x[1]] for x in currTrialsOracle]
+        
+        #save just first...?
+        validList = [validList[0]]
+        oracleList = [oracleList[0]]
+        
+        validVals = getDictMeans(validList)
+        oracleVals = getDictMeans(oracleList)
+        
+        resDataValid.append(validVals)
+        resDataOracle.append(oracleVals)
+    
+    
+    saveKeys = ['mcb', 'fitz', 'cologne', '385']
+    resValid = {}
+    resOracle = {}
+    
+    print('Valid:')
+    for elm in resDataValid[0].keys():
+        statVals = [x[elm] for x in resDataValid]
+        print(elm, Stats(statVals))
+        if elm in saveKeys:
+            resValid[elm] = Stats(statVals)
+    
+    print('\nOracle:')
+    for elm in resDataOracle[0].keys():
+        statVals = [x[elm] for x in resDataOracle]
+        print(elm, Stats(statVals))
+        if elm in saveKeys:
+            resOracle[elm] = Stats(statVals)
+    
+    return resValid, resOracle
+
+
+def printAllStats():
+    # folders = ['densenet121',
+    #  'densenet121-aug',
+    #  'densenet161',
+    #  'densenet161-aug',
+    #  'resnet18',
+    #  'resnet18-aug',
+    #  'resnet50',
+    #  'resnet50-aug',]
+    
+    folders = ['resnet18-no_aug',
+     'resnet18-aug',
+     'resnet18-no_aug-no_pretraining',
+     'resnet18-aug-no_pretraining',
+     'resnet50-no_aug',
+     'resnet50-aug',
+     'resnet50-no_aug-no_pretraining',
+     'resnet50-aug-no_pretraining',
+     'densenet121-no_aug',
+      'densenet121-aug',
+      'densenet161-no_aug',
+      'densenet161-aug']
+    
+    methods = ['resnet18',
+     'resnet18*',
+     'resnet18]',
+     'resnet18*]',
+     'resnet50',
+     'resnet50*',
+     'resnet50]',
+     'resnet50*]',
+     'densenet121',
+      'densenet121*',
+      'densenet161',
+      'densenet161*']
+    #TODO: update above with folder names and corresponding method names
+    
+    allValid = []
+    allOracle = []
+    for f in folders:
+        print(f)
+        tmpValid, tmpOracle = getStats(f + '/')
+        allValid.append(tmpValid)
+        allOracle.append(tmpOracle)
+        # print(tmpValid, tmpOracle)
+    saveKeys = ['mcb', 'fitz', 'cologne', '385']
+    
+
+    
+    print('\n\n\n\n')
+    print('VALID')
+    print(methods)
+    for elm in saveKeys:
+        print('----')
+        print(elm)
+        for indx, method in enumerate(methods):
+            # print('!', method)
+            # print(method, allValid[indx][elm][0])
+            # print(allValid[indx][elm])
+            # print(allValid[indx])
+            # print(method, allValid[indx][elm][0], allValid[indx][elm][1])
+            print(method + f'\t{allValid[indx][elm][0]:.4f}' + f'\t{allValid[indx][elm][1]:.4f}')
+    print('\n\nORACLE')
+    for elm in saveKeys:
+        print('----')
+        print(elm)
+        for indx, method in enumerate(methods):
+            # print(method, allValid[indx][elm][0], allOracle[indx][elm][1])
+            print(method + f'\t{allOracle[indx][elm][0]:.4f}' + f'\t{allOracle[indx][elm][1]:.4f}')
+    
+    print('\n---LaTeX Table---\n')
+    resStr = ''
+    for indx, method in enumerate(methods):
+        resStr += '\n' + method
+        for elm in saveKeys:
+            if elm == '385':
+                resStr += f' & ${allValid[indx][elm][0]:.1f} \pm {allValid[indx][elm][1]:.1f}$'
+        for elm in saveKeys:
+            if elm == '385':
+                resStr += f' & ${allOracle[indx][elm][0]:.1f} \pm {allOracle[indx][elm][1]:.1f}$'
+        resStr += '\\\\'
+    print(resStr)
+        
+
+printAllStats()
+# getStats('densenet121/') 
+            
+        
+        
+        
---
+++ analysis/analysis_sweep.py
@@ -0,0 +1,508 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 20:20:52 2024
+
+@author: walla
+"""
+
+
+import json
+import os
+import numpy as np
+
+ENVIRONMENT_LENGTHS = []
+
+#492, 98/394
+#2211, 442/1769
+#712, 142/670
+EVALUATION_WEIGHTS = [98,442,142]
+EVALUATION_WEIGHTS = [x/sum(EVALUATION_WEIGHTS) for x in EVALUATION_WEIGHTS]
+
+# ENV0_WEIGHTS = [1,1]
+ENV_SIZE = {'env0_in': 289,
+            'env0_out': 72,
+            'env1_in': 59,
+            'env1_out': 14,
+            'env2_in': 11,
+            'env2_out': 2,
+            'env3_in': 36,
+            'env3_out': 8}
+
+
+
+def getResultData(fp):
+    '''
+    given filepath of .jsonl file
+    returns a list, where each element is dict of checkpoint
+    '''
+    ret = []
+    with open(fp, 'r', encoding='utf-8') as j:
+        for line in j:
+            ret.append(json.loads(line)) 
+    return ret
+
+def bestValidationModel(d, weighted = False):
+    '''
+
+    Parameters
+    ----------
+    d : list of dictionaries giving training data stuff.
+
+    list index, acc, dictionary of results where best valid model is
+
+    '''
+    if weighted:
+        w = EVALUATION_WEIGHTS
+    else:
+        w = [1/3, 1/3, 1/3]
+    
+    currBestI = 0
+    currBestA = 0
+    for indx, cp in enumerate(d):
+        currAcc = 0
+        currAcc += cp['env4_out_acc']*w[0]
+        currAcc += cp['env5_out_acc']*w[1]
+        currAcc += cp['env6_out_acc']*w[2]
+        if currAcc > currBestA:
+            currBestA = currAcc
+            currBestI = indx
+    return currBestI, currBestA, d[currBestI]
+
+def getEnvAccuracy(d, num):
+    totalEnvNum = ENV_SIZE['env' + str(num) + '_in'] + ENV_SIZE['env' + str(num) + '_out']
+    return d['env' + str(num) + '_in_acc']*ENV_SIZE['env' + str(num) + '_in']/totalEnvNum + d['env' + str(num) + '_out_acc']*ENV_SIZE['env' + str(num) + '_out']/totalEnvNum
+
+
+def getEnvOracle(d, num):
+    totalEnvNum = ENV_SIZE['env' + str(num) + '_in'] + ENV_SIZE['env' + str(num) + '_out']
+    return d[-1]['env' + str(num) + '_in_acc']*ENV_SIZE['env' + str(num) + '_in']/totalEnvNum + d[-1]['env' + str(num) + '_out_acc']*ENV_SIZE['env' + str(num) + '_out']/totalEnvNum
+
+def Stats(d):
+    data = np.array(d)
+    mean = 100 * np.mean(list(data))
+    err = 100 * np.std(list(data) / np.sqrt(len(data)))
+    return mean, err
+
+
+rootdir = 'data' #TODO: update root directory
+experiments = os.listdir(rootdir)
+data = {}
+for exp in experiments:
+    exppath = os.path.join(rootdir, exp)
+    jsonlfile = os.path.join(exppath, 'results.jsonl')
+    jsonData = getResultData(jsonlfile)
+    info = exp.strip().split("_")
+    alg = info[1]
+    largeTrial = int(info[3][1:])
+    subTrial = int(info[2][1:])
+    if alg not in data:
+        data[alg] = {}
+    if largeTrial not in data[alg]:
+        data[alg][largeTrial] = {}
+    data[alg][largeTrial][subTrial] = jsonData
+    
+
+    
+
+
+algs = sorted(data.keys())
+
+
+r1 = ''
+r2 = ''
+d1 = {}
+d2 = {}
+
+'''
+MCB
+'''
+
+print('\n MCB \n')
+
+
+print('\n --- Unweighted Train Validation --- MCB')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            checkPoint, acc, subTrialData = bestValidationModel(staData)
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 0)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    
+
+
+print('\n --- Weighted Train Validation --- MCB')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            checkPoint, acc, subTrialData = bestValidationModel(staData, weighted=True)
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 0)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    d1[a] = {}
+    d1[a]['MCB'] = (mean, se)
+ 
+
+print('\n --- Oracle Validation --- MCB')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            acc = getEnvOracle(staData, 0)
+            checkPoint = -1
+            subTrialData = staData[-1]
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 0)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+        
+
+
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    d2[a] = {}
+    d2[a]['MCB'] = (mean, se)
+
+
+
+
+'''
+Fitz
+'''
+
+print('\n FITZ \n')
+
+print('\n --- Unweighted Train Validation --- Fitz')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            checkPoint, acc, subTrialData = bestValidationModel(staData)
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 1)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    
+# for a in sorted(res.keys()):
+#     print(a, sorted(res[a]))
+
+
+# print('\n\n--Details--')
+# for a in sorted(resDet.keys()):
+#     print(a, resDet[a])
+
+
+
+print('\nWeighted Train Validation --- Fitz')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            checkPoint, acc, subTrialData = bestValidationModel(staData, weighted=True)
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 1)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    d1[a]['Fitz'] = (mean, se)
+    
+# for a in sorted(res.keys()):
+#     print(a, sorted(res[a]))
+
+
+# print('\n\n--Details--')
+# for a in sorted(resDet.keys()):
+#     print(a, resDet[a])
+    
+
+
+print('\nOracle Validation --- Fitz')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            acc = getEnvOracle(staData, 1)
+            checkPoint = -1
+            subTrialData = staData[-1]
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 1)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+
+
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    d2[a]['Fitz'] = (mean, se)
+    
+
+
+'''
+Cologne
+'''
+
+print('\n COLOGNE \n')
+
+
+print('\n --- Unweighted Train Validation --- Cologne')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            checkPoint, acc, subTrialData = bestValidationModel(staData)
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 2)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+  
+
+print('\n --- Weighted Train Validation --- Cologne')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = 0
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            checkPoint, acc, subTrialData = bestValidationModel(staData, weighted=True)
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 2)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    d1[a]['Col'] = (mean, se)
+    
+
+
+
+print('\n --- Oracle Validation --- Cologne')
+
+res = {}
+resDet = {}
+for a in algs:
+    res[a] = []
+    trials = sorted(data[a].keys())
+    resDet[a] = {}
+    for t in trials:
+        bestAcc = -1
+        bestIndx = -1
+        currData = None
+        bestCheckPoint = -1
+        for sub in sorted(data[a][t].keys()):
+            staData = data[a][t][sub]
+            acc = getEnvOracle(staData, 2)
+            checkPoint = -1
+            subTrialData = staData[-1]
+            #can replace above with other model selection method
+            if acc > bestAcc:
+                bestAcc = acc
+                bestIndx = sub
+                currData = subTrialData
+                bestCheckPoint = checkPoint
+        bestTrialAccuracy = getEnvAccuracy(currData, 2)
+        res[a].append(bestTrialAccuracy)
+        resDet[a][t] = (bestIndx, currData['step'])
+        
+
+
+for a in sorted(res.keys()):
+    mean, se = Stats(res[a])
+    print(a, mean, se)
+    d2[a]['Col'] = (mean, se)
+
+def dictToTable(d):
+    # w = [400, 64, 13]
+    res = ''
+    for alg in sorted(d.keys()):
+        res += '\n' + alg
+        for domain in sorted(d[alg].keys(), reverse = True):
+            res += ' ' + domain
+            
+    res += '\n\n'
+    for alg in sorted(d.keys()):
+        res += '\n' + alg
+        for domain in sorted(d[alg].keys(), reverse = True):
+            res += '    & ' + '${0:.1f}'.format(d[alg][domain][0]) + ' \pm {0:.1f}$ '.format(d[alg][domain][1])
+        res += '\\\\'
+    return res
+
+
+r1 = dictToTable(d1)
+r2 = dictToTable(d2)
+
+print('\n\nValidation Selection')
+print(r1)
+
+
+print('\n\nOracle Selection')
+print(r2)
+
+
+def dictToChart(d):
+    res = ['' for _ in range(7)]
+    for alg in ['ERM',  'ERM-unfreeze', 'ERMNoAug', 'ERMRes18', 'ERM-unpretrained', 'IRM', 'GroupDRO', 'Mixup', 'MLDG', 'CORAL', 'MMD', 'DANN', 'CDANN', 'SagNet', 'EQRM']:
+        res[0] += alg + '\n'
+        res[1] += '{0:.4f}'.format(d[alg]['MCB'][0]) + '\n'
+        res[2] += '{0:.4f}'.format(d[alg]['Fitz'][0]) + '\n'
+        res[3] += '{0:.4f}'.format(d[alg]['Col'][0]) + '\n'
+        res[4] += '{0:.4f}'.format(d[alg]['MCB'][1]) + '\t'
+        res[5] += '{0:.4f}'.format(d[alg]['Fitz'][1]) + '\t'
+        res[6] += '{0:.4f}'.format(d[alg]['Col'][1]) + '\t'
+    return res
+
+
+print("\nValidation Accuracy")
+p = dictToChart(d1)
+for x in p:
+    print(x)
+
+print("\nOracle Accuracy")
+p = dictToChart(d2)
+for x in p:
+    print(x)
